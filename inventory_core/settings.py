274ee613cf799c4106b15a4b3a64fb8320a0a402
peteraggi <aggipeter25@gmail.com>
2026-07-24 12:16:41 +0300

    changes made

--- inventory_core/settings.py
+++ inventory_core/settings.py
@@ -258,7 +258,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "inventory_static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 
